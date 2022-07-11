07ade0ef8d8afcc0a8e9358a3213fb575caf2d6c
Jonathas <jonathasx57@gmail.com>
2022-07-11 20:33:48 -0300

    first component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,12 @@
+import NotificationButton from "./assets/components/notificationButton"
+
 function App() {
   return(
+    <>
     <h1>Olá mundo</h1>
+    <NotificationButton/>
+    <NotificationButton/>
+    </>
   )
 }
 
